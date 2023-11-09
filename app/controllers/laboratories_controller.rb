e8b48838ac7c559755c583e090ca4b2ad08e12bc
amirn003 <amir.naar@gmail.com>
2023-11-09 16:27:40 +0100

    add new create laboratory controller and view

--- app/controllers/laboratories_controller.rb
+++ app/controllers/laboratories_controller.rb
@@ -1,17 +1,33 @@
 class LaboratoriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @laboratories = Laboratory.all
   end
 
   def new
+    @laboratory = Laboratory.new
   end
 
   def create
+    @laboratory = Laboratory.new(laboratory_params)
+
+    if @laboratory.save
+      redirect_to @laboratory, notice: 'Created successfully!'
+    else
+      render :new
+    end
+
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def laboratory_params
+    params.require(:laboratory).permit(:name, :location)
   end
 end
