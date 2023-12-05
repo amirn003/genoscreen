26db09d3e23f6c8cdc16384fc4e55ab11dad356d
amirn003 <amir.naar@gmail.com>
2023-12-05 21:21:50 +0100

    add an argument to call the job

--- app/jobs/fake_job.rb
+++ app/jobs/fake_job.rb
@@ -1,7 +1,9 @@
 class FakeJob < ApplicationJob
   queue_as :default
 
-  def perform
+  def perform(user_id)
+    user = User.find(user_id)
+    puts "Calling API for #{user.username}..."
     puts "I'm starting the fake job"
     sleep 3
     puts "OK I'm done!"
