a5df6d3e3bcb2d3ceea0d6b8b9377b06d15f5c7d
amirn003 <amir.naar@gmail.com>
2023-12-06 21:55:53 +0100

    add user async update

--- app/jobs/fake_job.rb
+++ app/jobs/fake_job.rb
@@ -3,7 +3,7 @@ class FakeJob < ApplicationJob
 
   def perform(user_id)
     user = User.find(user_id)
-    puts "Calling API for #{user.username}..."
+    puts "Calling API for #{user.email}..."
     puts "I'm starting the fake job"
     sleep 3
     puts "OK I'm done!"
